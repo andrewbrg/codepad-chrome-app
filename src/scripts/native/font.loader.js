68a6b357201ad08aa7368e3842bd45031c752fbe
andrew <andrewbrg87@gmail.com>
2017-10-10 22:09:21 +0200

    Working on editor

--- src/scripts/native/font.loader.js
+++ src/scripts/native/font.loader.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     xhr.open("GET", "https://fonts.gstatic.com/s/robotomono/v4/hMqPNLsu_dywMa4C_DEpY44P5ICox8Kq3LLUNMylGO4.woff2", true);
     xhr.responseType = "blob";
     xhr.onreadystatechange = function () {
-        console.log("STATE", xhr.readyState);
         if (xhr.readyState === 4) {
             var myfontblob = window.URL.createObjectURL(xhr.response);
             $("<style>").text("@font-face {\
