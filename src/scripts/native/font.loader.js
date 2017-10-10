5e53290e67dd3b57a383ef78422b431967df00b9
andrew <andrewbrg87@gmail.com>
2017-10-10 22:08:48 +0200

    Working on editor

---
+++ src/scripts/native/font.loader.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://fonts.gstatic.com/s/robotomono/v4/hMqPNLsu_dywMa4C_DEpY44P5ICox8Kq3LLUNMylGO4.woff2", true);
+    xhr.responseType = "blob";
+    xhr.onreadystatechange = function () {
+        console.log("STATE", xhr.readyState);
+        if (xhr.readyState === 4) {
+            var myfontblob = window.URL.createObjectURL(xhr.response);
+            $("<style>").text("@font-face {\
+            font-family: 'Roboto Mono';\
+            font-style: normal;\
+            font-weight: 400;\
+            src: '" + myfontblob + "' format('woff2');\
+        }").prependTo("head");
+        }
+    };
+    xhr.send();
+});
