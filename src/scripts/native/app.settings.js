18735cc90ed4ba99303e6b22755850214c50d6ce
andrew <andrewbrg87@gmail.com>
2017-10-16 22:04:37 +0200

    Working on editor

--- src/scripts/native/app.settings.js
+++ src/scripts/native/app.settings.js
@@ -11,17 +11,51 @@ $(document).ready(function () {
         }
 
         var $that = $(this);
-        window.EditorTabInstance.aceEditors.forEach(function (el) {
-            switch ($that.attr('name')) {
-                case 'chk-word-wrap':
-                    el.getSession().setUseWrapMode($that.prop('checked'));
-                    break;
-                case 'chk-active-line':
-                    el.getSession().setHighlightActiveLine($that.prop('checked'));
-                    break;
-                case 'chk-print-margin':
-                    el.getSession().setShowPrintMargin($that.prop('checked'));
-                    break;
+        window.EditorTabInstance.aceEditors.forEach(function (aceEditor) {
+
+            if (typeof aceEditor !== typeof undefined) {
+
+                aceEditor      = aceEditor.ace;
+                var aceSession = aceEditor.getSession();
+
+                switch ($that.attr('name')) {
+                    case 'chk-highlightLine':
+                        aceSession.setHighlightActiveLine($that.prop('checked'));
+                        break;
+                    case 'chk-hideGutter':
+                        aceSession.setShowGutter(!$that.prop('checked'));
+                        break;
+                    case 'chk-showWhitespace':
+                        aceSession.setDisplayIndentGuides($that.prop('checked'));
+                        break;
+                    case 'chk-indentation':
+                        aceSession.setShowPrintMargin($that.prop('checked'));
+                        break;
+                    case 'chk-useTabs':
+                        aceSession.setShowPrintMargin($that.prop('checked'));
+                        break;
+                    case 'chk-wordWrap':
+                        aceSession.setShowPrintMargin($that.prop('checked'));
+                        break;
+                    case 'chk-wrapLimit':
+                        aceSession.setShowPrintMargin($that.prop('checked'));
+                        break;
+                    case 'chk-showMargin':
+                        aceSession.setShowPrintMargin($that.prop('checked'));
+                        break;
+                    case 'chk-lineEndings':
+                        aceSession.setShowPrintMargin($that.prop('checked'));
+                        break;
+                    case 'chk-scrollPastEnd':
+                        aceSession.setDisplayIndentGuides($that.prop('checked'));
+                        break;
+                    case 'chk-trimTrailingWhitespace':
+                        aceSession.setShowPrintMargin($that.prop('checked'));
+                        break;
+                    case 'chk-trimEmptyLines':
+                        aceSession.trimTrailingSpace($that.prop('checked'));
+                        break;
+                }
             }
         });
     });
