2a390e3c0848474a1f66179755f59acbf1f9ca63
andrew <andrewbrg87@gmail.com>
2017-10-18 23:33:07 +0200

    Working on editor

--- src/js/app.settings.js
+++ src/js/app.settings.js
@@ -2,83 +2,25 @@
 /// Initialisation application settings
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function () {
-    $(document).on('change', 'input[type="checkbox"]', function () {
+    $(document).on('change', 'input[data-option], select[data-option]', function () {
         if (typeof window.EditorTabInstance === typeof undefined ||
             typeof window.EditorTabInstance.aceEditors === typeof undefined) {
             return false;
         }
 
-        var $that    = $(this);
-        var value    = $that.val();
-        var attrName = $that.attr('name');
-        var checked  = $that.prop('checked');
-
+        var $that = $(this);
         window.EditorTabInstance.aceEditors.forEach(function (editor) {
 
             if (typeof editor === typeof undefined) {
-                return;
+                return false;
             }
 
-            var ace = editor.ace;
-            switch (attrName) {
+            var val = $that.attr('type') === 'checkbox'
+                ? $that.prop('checked')
+                : $that.val();
 
-                case 'sel-selectionStyle':
-                    ace.setOption('selectionStyle', value);
-                    break;
-
-                case 'sel-cursorStyle':
-                    ace.setOption('cursorStyle', value);
-                    break;
-
-                case 'chk-highlightActiveLine':
-                    ace.setOption('highlightActiveLine', checked);
-                    break;
-
-                case 'chk-showGutter':
-                    ace.setOption('showGutter', checked);
-                    break;
-
-                case 'chk-showInvisibles':
-                    ace.setOption('showInvisibles', checked);
-                    break;
-
-                case 'chk-displayIndentGuides':
-                    ace.setOption('displayIndentGuides', checked);
-                    break;
-
-                case 'chk-tabSize':
-                    ace.setOption('tabSize', value);
-                    break;
-
-                case 'chk-useSoftTabs':
-                    ace.setOption('tabSize', checked);
-                    break;
-
-                case 'chk-wrapBehavioursEnabled':
-                    ace.setOption('wrapBehavioursEnabled', checked);
-                    break;
-
-                case 'chk-wrap':
-                    ace.setOption('wrap', value);
-                    break;
-
-                case 'chk-showShowPrintMargin':
-                    ace.setOption('showPrintMargin', checked);
-                    break;
-                    
-                case 'chk-lineEndings':
-                    ace.setShowPrintMargin(checked);
-                    break;
-                case 'chk-scrollPastEnd':
-                    ace.setOption('scrollPastEnd', checked);
-                    break;
-                case 'chk-trimTrailingWhitespace':
-                    ace.setShowPrintMargin(checked);
-                    break;
-                case 'chk-trimEmptyLines':
-                    ace.trimTrailingSpace(checked);
-                    break;
-            }
+            editor.ace.setOption($that.attr('data-option').toString(), val);
+            editor.ace.$blockScrolling = 'Infinity';
         });
     });
 });
